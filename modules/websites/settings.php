8880c13ef5b9334a9199a31db8e750d4f35a1ad5
Nicolas Renault <nrenault@noriade.com>
2016-05-31 20:16:13 +0200

    edit color of tr

--- modules/websites/settings.php
+++ modules/websites/settings.php
@@ -1,7 +1,7 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
   <h1>
-    Data Tables
+    Websites
     <small>advanced tables</small>
   </h1>
   <ol class="breadcrumb">
@@ -16,10 +16,6 @@
   <div class="row">
     <div class="col-xs-12">
       <div class="box">
-        <div class="box-header">
-          <h3 class="box-title">Hover Data Table</h3>
-        </div>
-        <!-- /.box-header -->
         <div class="box-body">
           <table id="example2" class="table table-bordered table-hover">
             <thead>
