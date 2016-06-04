c39c2993f7d71269427b32503060b8378d2897ea
Nicolas Renault <nrenault@noriade.com>
2016-06-04 05:51:31 +0200

    edit

--- modules/ecommerce/brands.php
+++ modules/ecommerce/brands.php
@@ -74,7 +74,7 @@ function addCat(form) {
 </script>
 <!-- Content Header (Page header) -->
 <section class="content-header">
-  <h1>Categories</h1><br><div id="successMessage" style="font-size:15px;color:green;font-weight:bold;"></div>
+  <h1>Brands</h1><br><div id="successMessage" style="font-size:15px;color:green;font-weight:bold;"></div>
   <ol class="breadcrumb">
     <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
     <li><a href="#">E-Commerce</a></li>
@@ -156,7 +156,139 @@ function addCat(form) {
             </thead>
             <tbody>
               <?php
-              $sql_select_brands = "SELECT brand,COUNT(*) as count FROM products_fr GROUP BY brand ORDER by COUNT(*) desc limit 100";
+              $sql_select_brands = "SELECT brand,COUNT(*) as count FROM products_fr GROUP BY brand ORDER by COUNT(*) desc limit 50";
+              $req_select_brands = mysql_query($sql_select_brands) or die('<br>Erreur SQL !<br>'.$sql_select_brands.'<br>'.mysql_error());
+              while ($row_brands = mysql_fetch_assoc($req_select_brands)) {
+                  $brand = $row_brands['brand'];
+                  $count = $row_brands['count'];
+                  $sql_select_brands_infos = "SELECT * from brands where name = '$brand'";
+                  $req_select_brands_infos = mysql_query($sql_select_brands_infos) or die('<br>Erreur SQL !<br>'.$sql_select_brands_infos.'<br>'.mysql_error());
+                  if (mysql_num_rows($req_select_brands_infos)) {
+                    $brands_img = mysql_fetch_array($req_select_brands_infos);
+                    $brand_id = $brands_img['id'];
+                    $brand_img = $brands_img['img'];
+                    $brand_status = $brands_img['active'];
+                  } else {
+                    $brand_id = '';
+                    $brand_img = '';
+                    $brand_status = '';
+                  }
+                  echo "<tr><td>";
+                  echo $brand_id;
+                  echo "</td><td>";
+                  echo $brand;
+                  echo "</td><td>";
+                  echo $brand_img;
+                  echo "</td><td>";
+                  echo $count;
+                  echo "</td><td align='center'>";
+                  if ($brand_status == "1") {
+                    echo "<font color='green'><span class='glyphicon glyphicon-ok-circle' aria-hidden='true'></span></font>";
+                    echo "</td><td>";
+                    $confirm_desactivate = "confirmDesactivateModal('".$node_id."')";
+                    echo '<button type="button" class="btn btn-xs btn-warning" onclick="'.$confirm_desactivate.'"><i class="fa fa-ban"></i> Desactivate</button>';
+                  } else {
+                    echo "<font color='red'><span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span></font>";
+                    echo "</td><td>";
+                    $confirm_activate = "confirmActivateModal('".$node_id."')";
+                    echo '<button type="button" class="btn btn-xs btn-success" onclick="'.$confirm_activate.'"><i class="fa fa-check"></i> Activate</button>';
+                  }
+                  $confirm_delete = "confirmDeleteModal('".$node_id."')";
+                  echo "&nbsp;&nbsp;";
+                  echo '<button type="button" class="btn btn-xs btn-danger" onclick="'.$confirm_delete.'"><i class="glyphicon glyphicon-remove"></i> Delete</button>';
+                  echo "</td><tr>";
+              }
+              ?>
+            </tbody>
+            <tfoot>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Img</th>
+              <th>Products</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+            </tfoot>
+          </table>
+        </div>
+
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+    <div class="col-xs-6">
+      <div id='desactivateModal' class='modal fade' role='dialog'>
+        <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>
+              <h4 class='modal-title'>Desactivate </h4>
+            </div>
+            <div class='modal-body'>
+              <p>Do You Really Want to Desactivate This ?</p>
+            </div>
+            <div class='modal-footer'>
+              <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
+                  <span id= 'desactivateButton'></span>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div id='activateModal' class='modal fade' role='dialog'>
+        <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>
+              <h4 class='modal-title'>Activate </h4>
+            </div>
+            <div class='modal-body'>
+              <p>Do You Really Want to Activate This ?</p>
+            </div>
+            <div class='modal-footer'>
+              <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
+                  <span id= 'activateButton'></span>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div id='deleteModal' class='modal fade' role='dialog'>
+        <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>
+              <h4 class='modal-title'>Delete </h4>
+            </div>
+            <div class='modal-body'>
+              <p>Do You Really Want to Delete This ?</p>
+            </div>
+            <div class='modal-footer'>
+              <button type='button' class='btn btn-default' data-dismiss='modal'>Cancel</button>
+                  <span id= 'deleteButton'></span>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="box">
+        <!-- <div class="box-header">
+          <h3 class="box-title">Hover Data Table</h3>
+        </div> -->
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="example2" class="table table-bordered table-hover">
+            <thead>
+            <tr>
+              <th>id</th>
+              <th>Name</th>
+              <th>Img</th>
+              <th>Products</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+            </thead>
+            <tbody>
+              <?php
+              $sql_select_brands = "SELECT brand,COUNT(*) as count FROM products_fr GROUP BY brand ORDER by COUNT(*) desc limit 50";
               $req_select_brands = mysql_query($sql_select_brands) or die('<br>Erreur SQL !<br>'.$sql_select_brands.'<br>'.mysql_error());
               while ($row_brands = mysql_fetch_assoc($req_select_brands)) {
                   $brand = $row_brands['brand'];
