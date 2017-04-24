bf00993c5a3adf5dcd8596009b6be8602aabe89e
Nicolas Renault <nrenault@noriade.com>
2017-04-24 20:25:19 +0200

    edit

--- modules/ecommerce/brands.php
+++ modules/ecommerce/brands.php
@@ -74,7 +74,7 @@ function addCat(form) {
         <!-- /.box-header -->
         <div class="box-body">
           <?php
-          if($dossier = opendir('./mondossier')) {
+          if($dossier = opendir('images/brands/')) {
             while(false !== ($fichier = readdir($dossier))) {
               if($fichier != '.' && $fichier != '..' && $fichier != 'index.php') {
                 echo $fichier;
